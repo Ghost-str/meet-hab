ab2a2b037c5b64bf20df52dca47346d06de03b03
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-06 22:56:54 +0700

    login and register methoods

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 import makeSwaggerConfig from './config/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { VALIDATION_PIPE_CONFIG } from './config/validation.pipe';
+import cookie, { FastifyCookieOptions } from '@fastify/cookie';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,6 +17,11 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe(VALIDATION_PIPE_CONFIG));
+
+  app.register(cookie, {
+    secret: process.env.BACKEND_APP_KEY,
+  } as FastifyCookieOptions);
+
   makeSwaggerConfig(app);
 
   await app.listen(3000, '0.0.0.0');
--- src/backend/src/modules/users/services/auth/auth.service.ts
+++ src/backend/src/modules/users/services/auth/auth.service.ts
@@ -1,18 +1,70 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { CredentialsDto } from '../../dto/credentials.dto';
+import { User } from '../../entities/user.entity';
+import jwt from 'jsonwebtoken';
+import { scrypt } from 'node:crypto';
+import { CreateUserDto } from '../../dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  protected appKey: string;
+  protected hashLength = 25;
+  protected expiresIn = 60 * 60 * 27 * 7;
+
+  constructor(private usersService: UsersService) {
+    const key = process.env.BACKEND_APP_KEY;
+    if (!key) {
+      throw new Error('BACKEND_APP_KEY not set');
+    }
+
+    this.appKey = key;
+  }
 
   async login(data: CredentialsDto) {
-    const user = await this.usersService.findByLogin(data.login);
+    const [user, passwordHash] = await Promise.all([
+      this.usersService.findByLogin(data.login),
+      this.makePasswordHash(data.password),
+    ]);
 
-    if (!user || user.password !== data.password) {
+    if (!user || user.password !== passwordHash) {
       return null;
     }
 
-    return user;
+    return {
+      authKey: this.makeAuthKey(user),
+      user,
+    };
+  }
+
+  async register(newUser: CreateUserDto) {
+    const passwordHash = await this.makePasswordHash(newUser.password);
+
+    return this.usersService.create({
+      ...newUser,
+      password: passwordHash,
+    });
+  }
+
+  protected async makePasswordHash(password: string): Promise<string> {
+    return new Promise((resolv, reject) => {
+      scrypt(password, this.appKey, this.hashLength, (err, key) => {
+        if (err) {
+          return reject(err);
+        } else {
+          return resolv(key.toString('utf-8'));
+        }
+      });
+    });
+  }
+
+  protected makeAuthKey(user: User): string {
+    return jwt.sign(
+      {
+        id: user.id,
+      },
+      this.appKey,
+      { expiresIn: this.expiresIn },
+    );
   }
 }
