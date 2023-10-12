8cb38e57c86b609f56ee256dddea3466da6c0a1e
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-13 04:37:01 +0700

    fix frontend build

--- src/backend/src/config/database.ts
+++ src/backend/src/config/database.ts
@@ -4,7 +4,7 @@ import { DataSourceOptions } from 'typeorm';
 export const config: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: +(process.env.POSTGRES_PORT || 3000),
+  port: +(process.env.POSTGRES_PORT || 5432),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
