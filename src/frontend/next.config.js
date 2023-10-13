b2d9c0a1156ec2fb18a951af202fcd5ddb290f1f
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-13 17:38:07 +0700

    fix frontend config

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -3,8 +3,14 @@ const path = require('node:path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    compress: false,
+    poweredByHeader: false,
+    reactStrictMode: true,
     experimental: {
         outputFileTracingRoot: path.join(__dirname, '../'),
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
     },
 };
 
