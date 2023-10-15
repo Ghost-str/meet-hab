48127b443d08a86499868b9ee98629d91e942b28
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-15 16:17:27 +0700

    debug

--- docker/dockersend.sh
+++ docker/dockersend.sh
@@ -2,18 +2,29 @@
 
 imageIds=$(docker images -f "label=com.docker.compose.project=meet-hub" -f "dangling=false" --format "{{.ID}} {{.Repository}} {{.Tag}}")
 
+echo "$imageIds"
+
 for line in $imageIds; do
+    echo "Line:   $line"
     dockerId=$(echo "$line" | awk '{print $1}')
     repository=$(echo "$line" | awk '{print $2}')
     tag=$(echo "$line" | awk '{print $3}')
-
-    echo "processing container with id: $dockerId"
-    docker save "$dockerId" | bzip2 | ssh server 'bunzip2 | docker load'
-    echo "tagging image as $repository:$tag"
-    ssh server "docker tag $dockerId $repository:$tag"
+    echo "items dockerid: $dockerId  repository: $repository  tag: $tag"
 done
 
-echo "up new containers"
-DOCKER_HOST="ssh://server" docker compose -f docker-compose up -d --remove-orphans
-echo "clean old containers"
-DOCKER_HOST="ssh://server" docker system prune -f -a
+
+# for line in $imageIds; do
+#     dockerId=$(echo "$line" | awk '{print $1}')
+#     repository=$(echo "$line" | awk '{print $2}')
+#     tag=$(echo "$line" | awk '{print $3}')
+
+#     echo "processing container with id: $dockerId"
+#     docker save "$dockerId" | bzip2 | ssh server 'bunzip2 | docker load'
+#     echo "tagging image as $repository:$tag"
+#     ssh server "docker tag $dockerId $repository:$tag"
+# done
+
+# echo "up new containers"
+# DOCKER_HOST="ssh://server" docker compose -f docker-compose up -d --remove-orphans
+# echo "clean old containers"
+# DOCKER_HOST="ssh://server" docker system prune -f -a
