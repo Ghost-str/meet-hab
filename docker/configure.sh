12fefff8a9c248a31d38e736ebc340eeb171408b
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-15 15:53:06 +0700

    try

---
+++ docker/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir ~/.ssh
+cat > ~/.ssh/config << EOF 
+Host server
+    HostName $1
+    User $2
+    Port $3
+    PubKeyAuthentication yes
+    IdentityFile ~/.ssh/private_key
+EOF
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/config
