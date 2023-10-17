411aab15931407b25ab18a472ecc6ff953e27dcd
ghost-str <Ghost-str@users.noreply.github.com>
2023-10-17 18:21:22 +0700

    fix

--- src/backend/src/seeds/seeder.ts
+++ src/backend/src/seeds/seeder.ts
@@ -1,10 +1,13 @@
 import { seeder } from 'nestjs-seeder';
-import { TYPE_ORM } from '../config/database';
+import { makeTypeOrmModule } from '../config/database';
 import { ConfigModule } from '@nestjs/config';
 import { User } from '../modules/users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import UsersSeeder from './users-seeder';
 import { UsersModule } from '../modules/users/users.module';
+
+
+const TYPE_ORM = makeTypeOrmModule();
 
 seeder({
   imports: [
